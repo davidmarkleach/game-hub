1e75c4736c5758cf69a605a7ce0bcc67449a01e8
David Leach <davidmarkleach@gmail.com>
2024-02-11 15:03:30 -0500

    initial commit!

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default axios.create({
-  baseURL: "https://api.rawg.io/api",
-  params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
-  },
+	baseURL: 'https://api.rawg.io/api',
+	params: {
+		key: 'f08a9284de054e3f8f55a73b827fc2b0',
+	},
 });
